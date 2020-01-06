c2a0e0df95d487a1fe73ad4e1571dd4ef006e36a
RenatoIgor <renatoigor10@yahoo.com.br>
2020-01-06 18:15:25 -0300

    versao 1.0 inserir boa mensagem

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+using namespace std;
+
+void boa_mensagem() {
+
+    cout << "Ola, tudo bem!" <<endl;
+}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    boa_mensagem();
     return 0;
 }
